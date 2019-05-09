abee168fdcb0c1cff4d8c5d2c73d590b245a4a6d
Juan Castelli <juancastelli12345@gmail.com>
2019-05-09 19:41:52 -0300

    Pusheando ejercicio 1

---
+++ Ejercicio 1/Ejercicio 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 501;
+            int aux;
+        while(num > 0)
+           {
+               if(num < 1000)
+               {
+                   aux = num % 10;
+                   num = num / 10;
+               }
+               if(num < 1000)
+               {
+                   aux = num % 100;
+                   num = num / 100;
+               }   
+                          
+           }
+                  
+    }
+}
